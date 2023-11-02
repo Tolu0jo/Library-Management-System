f0fa93f7168eca54c2e7f090e7171f70bdcfd066
toluOjo <ojotolulope137@gmail.com>
2023-11-02 10:39:52 +0100

    database configuration

--- src/config/typeOrm.config.ts
+++ src/config/typeOrm.config.ts
@@ -1,12 +1,7 @@
 import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 
 export const typeOrmConfig:TypeOrmModuleOptions={
-type:"mysql",
-host: "localhost",
-port:3306,
-username: "root",
-password: "12345678",
-database:"library",
+type:"sqlite",
 entities:[__dirname + '/../**/*.entity{.ts,.js}'],
 synchronize:true
 }
